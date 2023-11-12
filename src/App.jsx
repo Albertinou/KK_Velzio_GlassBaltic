7cc3f0585cbf3deef3c3475a1fa82d6d0f579adb
Albertinou Albertinou <albertuciukas@gmail.com>
2023-11-12 10:35:17 +0200

    removed header/footer from error pg

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,23 @@ import Players from "./Pages/Players"
 import SingleNews from "./Pages/SingleNews"
 import Error from "./Pages/Error"
 import Sponsors from "./Pages/Sponsors"
+import PageLayout from './components/PageLayout'
 
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Header />
         <Routes>
-          <Route path='/' exact element={<Homepage />} />
-          <Route path='/news' element={<News />} />
-          <Route path='/news/:slug' element={<SingleNews />} />
-          <Route path='/players' element={<Players />} />
-          <Route path='/sponsors' element={<Sponsors />} />
+          <Route element={<PageLayout />}>
+            <Route path='/' exact element={<Homepage />} />
+            <Route path='/news' element={<News />} />
+            <Route path='/news/:slug' element={<SingleNews />} />
+            <Route path='/players' element={<Players />} />
+            <Route path='/sponsors' element={<Sponsors />} />
+          </Route>
           <Route path='*' element={<Error />} />
         </Routes>
-        <Footer />
       </BrowserRouter>
     </>
 
