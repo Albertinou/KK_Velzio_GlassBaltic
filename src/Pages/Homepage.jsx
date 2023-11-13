a57632798016534325f79e075164295db71d26ed
Albertinou Albertinou <albertuciukas@gmail.com>
2023-11-13 14:29:16 +0200

    added playerslider component

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -4,6 +4,7 @@ import { client } from "../client";
 import { format } from 'date-fns'
 import { Link } from 'react-router-dom';
 import imageUrlBuilder from '@sanity/image-url';
+import PlayerSlider from '../components/PlayerSlider';
 
 const builder = imageUrlBuilder(client)
 
@@ -92,7 +93,7 @@ export default function Homepage() {
           }
         </>}
 
-      {/* ------------naujienu gridas-------------- */}
+      {/* ------------Naujienu gridas-------------- */}
 
       <section className='grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3 max-w-7xl mx-auto px-5 mb-10'>
         {news.map((singleNews) => (
@@ -120,36 +121,16 @@ export default function Homepage() {
           </article>
         ))}
       </section>
-      <div className='max-w-7xl mx-auto px-5 mb-20'>
+      <div className='max-w-7xl mx-auto px-5'>
         <Link to="/news" className='bg-white py-2 px-8 rounded shadow text-slate-800 tracking-wide hover:opacity-75 transition-all duration-200 md:w-auto'>
           Skaityti visas naujienas
         </Link>
       </div>
 
 
-      {/* ------------KOMANDA----------- */}
+      {/* ------------Zaideju slideris----------- */}
 
-      <h1 className='max-w-7xl text-3xl mx-auto py-20 px-5'>
-        KOMANDA
-      </h1>
-      {!players ? <h2 className='max-w-7xl text-3xl mx-auto pt-20 px-5'>
-        Loading...
-      </h2> :
-        <>
-          <section className='grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3 max-w-7xl mx-auto px-5 mb-10'>
-            {players.map((player) => (
-              <article key={player.slug.current}>
-                {player.image && <img
-                  src={urlFor(player.image).width(1000).height(1000).url()}
-                  alt={player.name}
-                  loading="lazy"
-                  className='rounded-xl md:h-90 w-full object-cover'
-                />}
-              </article>
-            ))}
-          </section>
-        </>}
-
+      <PlayerSlider />
 
     </>
   )
--- src/components/PlayerSlider.jsx
+++ src/components/PlayerSlider.jsx
@@ -1,7 +1,100 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { client } from "../client";
+import imageUrlBuilder from '@sanity/image-url'
+import { Swiper, SwiperSlide } from "swiper/react"
+
+import "swiper/css"
+import "swiper/css/pagination"
+import "swiper/css/free-mode"
+
+import { FreeMode, Pagination } from "swiper/modules"
+
+
+const builder = imageUrlBuilder(client)
+
+function urlFor(source) {
+    return builder.image(source)
+}
 
 export default function PlayerSlider() {
-  return (
-    <div>PlayerSlider</div>
-  )
+
+    const [players, setPlayers] = useState([]);
+
+    useEffect(() => {
+        client.fetch(
+            `*[_type == "player"] {
+        name,
+        slug,
+        bio,
+        image {
+          asset -> {
+            _id,
+            url
+          },
+          alt,
+        },
+      }`
+        ).then((data) => {
+            setPlayers(data);
+        }).catch(console.error);
+
+    }, [])
+
+
+
+    return (
+        <>
+            <h1 className='max-w-7xl text-3xl mx-auto py-20 px-5'>
+                KOMANDA
+            </h1>
+
+            <div>
+                <Swiper
+                    breakpoints={{
+                        340: {
+                            slidesPerView: 2,
+                            spaceBetween: 15
+                        },
+                        700: {
+                            slidesPerView: 3,
+                            spaceBetween: 15
+                        }
+                    }}
+
+                    freeMode={true}
+                    pagination={{
+                        clickable: true
+                    }}
+                    modules={[FreeMode, Pagination]}
+                    className='grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3 max-w-7xl mx-auto px-5 mb-10'
+                >
+
+                    {!players ? <h2 className='max-w-7xl text-3xl mx-auto pt-20 px-5'>
+                        Loading...
+                    </h2> :
+                        <>
+                            <section className='grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3 max-w-7xl mx-auto px-5 mb-10'>
+                                {players.map((player) => (
+                                    <SwiperSlide key={player.slug.current}>
+                                        {player.image && <img
+                                            src={urlFor(player.image).width(1000).height(1000).url()}
+                                            alt={player.name}
+                                            loading="lazy"
+                                            className='rounded-xl md:h-90 w-full object-cover'
+                                        />}
+                                    </SwiperSlide>
+                                ))}
+                            </section>
+                        </>}
+                </Swiper>
+            </div>
+
+
+
+
+
+        </>
+    )
 }
+
+
