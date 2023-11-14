a3e3f7be8cdde06ced6cc48be8c1951d5d1a8a23
Albertinou Albertinou <albertuciukas@gmail.com>
2023-11-14 16:31:47 +0200

    schedule component css

--- src/components/Schedule.jsx
+++ src/components/Schedule.jsx
@@ -58,45 +58,49 @@ export default function Schedule() {
             </h1>
             <section className='grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3 max-w-7xl mx-auto px-5 mb-10'>
                 {news.map((game) => (
-                    <article key={game.slug.current} className='border border-blue-700 rounded-xl flex'>
+                    <article key={game.slug.current} className='border border-blue-700 rounded-xl flex bg-white align-middle justify-between'>
 
-                        <div>
-                            {game.homeTeamImage && <img
-                                src={urlFor(game.homeTeamImage).fit("clip").size(500, 500).url()}
-                                alt={game.homeTeam.name}
-                                loading="lazy"
-                                className='rounded-xl md:h-32 h-32 w-32 object-cover'
-                            />}
+                        <div className=' p-2 flex justify-between items-center w-full'>
+                            
+                            <div className='flex flex-col justify-center items-center'>
+                                {game.homeTeamImage && <img
+                                    src={urlFor(game.homeTeamImage).fit("clip").size(500, 500).url()}
+                                    alt={game.homeTeam.name}
+                                    loading="lazy"
+                                    className='rounded-xl md:h-32 h-32 w-32 object-cover'
+                                />}
 
-                            <div className='py-4'>
-                                <p className='text-sm'>
-                                    {game.homeTeam}
-                                </p>
+                                <div className='py-4'>
+                                    <p className='text-sm'>
+                                        {game.homeTeam}
+                                    </p>
+                                </div>
+
                             </div>
 
-                        </div>
+
+                            <div className='py-4 flex flex-col justify-center items-center'>
+                                <p>
+                                    {format(new Date(game.publishedAt), 'yyyy-MM-dd')}
+                                </p>
+                                <p>{format(new Date(game.publishedAt), 'HH:mm')}</p>
+                            </div>
 
 
-                        <div className='py-4'>
-                            <p className='text-sm'>
-                                {format(new Date(game.publishedAt), 'yyyy-MM-dd HH:mm')}
-                            </p>
-                        </div>
+                            <div className='flex flex-col justify-center items-center'>
+                                {game.awayTeamImage && <img
+                                    src={urlFor(game.awayTeamImage).fit("clip").size(500, 500).url()}
+                                    alt={game.awayTeam.name}
+                                    loading="lazy"
+                                    className='rounded-xl md:h-32 h-32 w-32 object-cover'
+                                />}
 
-
-                        <div>
-                        {game.awayTeamImage && <img
-                            src={urlFor(game.awayTeamImage).fit("clip").size(500, 500).url()}
-                            alt={game.awayTeam.name}
-                            loading="lazy"
-                            className='rounded-xl md:h-32 h-32 w-32 object-cover'
-                        />}
-
-                        <div className='py-4'>
-                            <p className='text-sm'>
-                                {game.awayTeam}
-                            </p>
-                        </div>
+                                <div className='py-4'>
+                                    <p className='text-sm'>
+                                        {game.awayTeam}
+                                    </p>
+                                </div>
+                            </div>
                         </div>
 
                     </article>
