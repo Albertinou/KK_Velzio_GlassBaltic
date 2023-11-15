1ca8c859fd6886c4f09cfd550946d83508b85351
Albertinou Albertinou <albertuciukas@gmail.com>
2023-11-15 13:40:42 +0200

    added sponsors schema/page

--- kkvelzioglassbaltic/schemas/index.js
+++ kkvelzioglassbaltic/schemas/index.js
@@ -3,6 +3,7 @@ import post from './post'
 import player from './player'
 import komandos from "./komandos"
 import schedule from "./schedule"
+import sponsors from './sponsors'
 
 
-export const schemaTypes = [post, player, komandos, schedule, blockContent]
+export const schemaTypes = [post, player, komandos, schedule, sponsors, blockContent]
--- src/Pages/Sponsors.jsx
+++ src/Pages/Sponsors.jsx
@@ -1,7 +1,67 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { client } from "../client";
+import imageUrlBuilder from '@sanity/image-url'
+
+const builder = imageUrlBuilder(client)
+
+function urlFor(source) {
+  return builder.image(source)
+}
 
 export default function Sponsors() {
+
+  const [sponsors, setSponsors] = useState([]);
+
+  useEffect(() => {
+    client.fetch(
+      `*[_type == "sponsors"] {
+        name,
+        slug,
+        bio,
+        image {
+          asset -> {
+          _id,
+          url
+        },
+          crop,
+          hotspot,
+          alt
+        },
+      }`
+    ).then((data) => {
+      setSponsors(data);
+      console.log(data)
+    }).catch(console.error);
+
+  }, [])
+
+
+
   return (
-    <div>Sponsors</div>
+    <>
+
+      <h1 className='max-w-7xl text-3xl mx-auto pt-20 pb-10 px-5'>
+        RĖMĖJAI
+      </h1>
+      {!sponsors ? <h2 className='max-w-7xl text-3xl mx-auto pt-20 px-5'>
+        Loading...
+      </h2> :
+        <>
+
+          <section className='grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3 max-w-7xl mx-auto px-5 mb-10 place-items-center'>
+            {sponsors.map((sponsor) => (
+              <article key={sponsor.slug.current}>
+                {sponsor.image && <img 
+                  src={urlFor(sponsor.image).url()}
+                  alt={sponsor.name}
+                  loading="lazy"
+                  className='rounded-xl object-contain md:h-80'
+                />}
+              </article>
+            ))}
+          </section>
+        </>}
+    </>
   )
 }
+
