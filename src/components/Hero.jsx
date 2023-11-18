1c03ba90df0e0874fdd728248279ae89a66e0e85
Albertinou Albertinou <albertuciukas@gmail.com>
2023-11-18 23:47:31 +0200

    links in hero banner

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import { AiFillFacebook, AiFillMail, AiFillInstagram } from "react-icons/ai"
 
 export default function Hero() {
   return (
     <>
-      <section className='w-full bg-center bg-cover'>
+      <section className="relative bg-cover bg-center bg-no-repeat">
         <img src="./IMG_0133.jpeg" alt="hero banner" />
+        <div className='absolute top-[10%] left-[50%]'>
+          <div className='flex flex-col'>
+            <div className='pb-2'>
+              <a href="https://www.facebook.com/KKVelzioGlassBaltic" target="_blank" className='flex items-center'>
+                <AiFillFacebook className=' lg:text-5xl text-2xl text-white' />
+                <p className='pl-2 lg:text-3xl text-md text-white'>KK Velžio "GlassBaltic"</p>
+              </a>
+            </div>
+            <div>
+              <a href="https://www.instagram.com/kkvelzioglassbaltic/" target="_blank" className='flex items-center'>
+                <AiFillInstagram className=' lg:text-5xl text-2xl text-white' />
+                <p className='pl-2 lg:text-3xl text-md text-white'>kkvelzioglassbaltic</p>
+              </a>
+            </div>
+          </div>
+        </div>
       </section>
     </>
   )
