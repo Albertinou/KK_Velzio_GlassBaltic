6c50d8893c9f287da4e6a1009d6a23a04d6f68fb
Albertinou Albertinou <albertuciukas@gmail.com>
2023-11-18 20:51:42 +0200

    smoe css

--- src/components/Parama.jsx
+++ src/components/Parama.jsx
@@ -3,21 +3,16 @@ import React from 'react'
 export default function Parama() {
     return (
         <>
-            <div className='  max-w-7xl mx-auto px-5'>
+            <div className='  max-w-7xl mx-auto px-5 pt-10'>
                 <div className='px-5 border border-blue-700 rounded-xl  bg-white   shadow-md  shadow-gray-700 '>
                     <p className='pt-5 text-2xl uppercase font-bold'>Prisidėkite prie krepšinio klubo Velžys skirdami 1.2% GPM</p>
-                    <p className='pt-5 text-2xl'> Skirdami 1.2% paramą, Jūs prisidedate prie krepšinio klubo Velžys gyvavimo. </p>
-
+                    <p className='pt-6 text-2xl'> Skirdami 1.2% paramą, Jūs prisidedate prie krepšinio klubo Velžys gyvavimo. </p>
                     <p className='pt-2 text-2xl'> Prašymą FR0512 (5 versiją) pateikti galite tik elektroniniu būdu per VMI Elektroninio deklaravimo sistemą (EDS).</p>
-
                     <p className='pt-2 text-2xl'>Prašymus dėl 1.2% paramos galima teikti kiekvienais metais iki gegužės 2 d. </p>
-                    <p className='pt-2 text-2xl'>Rekvizitai prašymams dėl 1.2% pajamų mokesčio pervedimo:</p>
-
-                    <p className='pt-2 text-2xl'>Krepšinio klubas Velžys</p>
-
-                    <p className='pt-2 text-2xl'>Įstaigos kodas: 306440408</p>
-
-                    <p className='pt-2 pb-5 text-2xl'>Adresas: Nevėžio g. 54-1, Velžio k., 38129, Panevėžio r.</p>
+                    <p className='pt-5 text-2xl'>Rekvizitai prašymams dėl 1.2% pajamų mokesčio pervedimo:</p>
+                    <p className='pt-3 text-2xl text-blue-900'>Krepšinio klubas Velžys</p>
+                    <p className='pt-2 text-2xl text-blue-900'>Įstaigos kodas: 306440408</p>
+                    <p className='pt-2 pb-5 text-2xl text-blue-900'>Adresas: Nevėžio g. 54-1, Velžio k., 38129, Panevėžio r.</p>
                 </div>
             </div>
         </>
